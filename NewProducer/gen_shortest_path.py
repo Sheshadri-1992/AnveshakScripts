62a4e46ff68af5c8d885c817e265f7c88177b138
Sheshadri-1992 <sheshadri.madhu@gmail.com>
2019-05-05 22:23:12 +0530

    get shortest path added

---
+++ NewProducer/gen_shortest_path.py
@@ -0,0 +1,42 @@
+from collections import OrderedDict
+import networkx as nx
+import osmnx as ox
+import types
+from networkx.readwrite import json_graph
+import json
+import pickle
+import xmltodict
+import random
+import sumolib
+
+
+def compute_shortest_path(origin, dest, graph):
+    """
+
+    :param origin:
+    :param dest:
+    :param graph:
+    :return:
+    """
+    origin = '249072983'
+    dest = '448116544'
+    path = nx.shortest_path(graph, origin, dest, weight='weight')
+
+    # get edges
+
+    edges = [-1]
+
+    for i in range(len(path) - 1):
+        e = g[path[i]][path[i + 1]]
+        if type(e) is list:
+            for j in e:
+                if edges[-1] != j.keys()[0]:
+                    edges.append(j.keys()[0])
+        else:
+            if edges[-1] != e.keys()[0]:
+                edges.append(e.keys()[0])
+
+    edges = edges[1:]
+
+    print("The edge in the shortest path ", edges)
+    return edges
